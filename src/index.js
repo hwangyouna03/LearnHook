f17f07cb07d6482ebaa7ac8bac03eb1e2302146f
Yuna Hwang <57125986+hwangyouna03@users.noreply.github.com>
2021-05-29 23:02:53 +0900

    useEffect 실습

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@
 //import './use-state-demo/counter-with-hook-caveat.js'
 //import './use-state-demo/use-state-object-manage-caveat.js'
 //import './use-state-demo/theme-component-demo.js'
-import './use-state-demo/todo-app-demo.js'
+//import './use-state-demo/todo-app-demo.js'
+
+import './use-effect-demo/use-effect-deps'
