e949efdefe59ed14db8f28507e21135544c74075
Yuna Hwang <57125986+hwangyouna03@users.noreply.github.com>
2021-05-31 11:02:43 +0900

    useEffect - 의존배열 활용

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@
 //import './use-state-demo/theme-component-demo.js'
 //import './use-state-demo/todo-app-demo.js'
 
-import './use-effect-demo/use-effect-deps'
+//import './use-effect-demo/use-effect-deps'
+//import './use-effect-demo/use-effect-intro'
+import './use-effect-demo/use-effect-using-local-storage'
